d321cd84641b895982a17a07cf24b44db3342ca6
Joseph <joseph.larson2@gmail.com>
2018-04-04 22:08:22 -0600

    update old plane reducer

--- src/cs455/hadoop/olderplanes/OldPlaneDelaysReducer.java
+++ src/cs455/hadoop/olderplanes/OldPlaneDelaysReducer.java
@@ -1,5 +1,6 @@
 package cs455.hadoop.olderplanes;
 
+import cs455.hadoop.customresearch.FlightCounts;
 import cs455.hadoop.utils.MapSorts;
 import cs455.hadoop.utils.TypeCheckUtil;
 import org.apache.hadoop.io.IntWritable;
@@ -19,58 +20,35 @@ import java.util.Map;
  */
 public class OldPlaneDelaysReducer extends Reducer<Text, Text, Text, LongWritable> {
 
-    private long totalFlightsOver20years = 0;
-    private long delayedFlightsOver20years=0;
-    private long totalFlightsUnder20years = 0;
-    private long delayedFlightsUnder20years = 0;
+    private Map<Text, Integer> countMap = new HashMap<>();
+    private Map<Text, Integer> airplanes = new HashMap<>();
 
-    private void processFlight(int yearOfFlight, int planeYear, boolean hadDelay) {
-        if(yearOfFlight - planeYear > 20) {
-            totalFlightsOver20years++;
-            if(hadDelay) {
-                delayedFlightsOver20years++;
-            }
-        } else {
-            totalFlightsUnder20years++;
-            if(hadDelay) {
-                delayedFlightsUnder20years++;
-            }
-        }
-    }
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) {
-        int year =0;
-        ArrayList<Integer> flightsToBeProcessed = new ArrayList<>();
+
         for(Text value : values) {
             String[] fileSplit = value.toString().split("-");
             if("f2".equalsIgnoreCase(fileSplit[0]) && TypeCheckUtil.isInteger(fileSplit[1])) {
-                year = Integer.parseInt(fileSplit[1]);
-                if(flightsToBeProcessed.size() > 0) {
-                    for(int i = 0; i < flightsToBeProcessed.size(); i++) {
-                        int flight = flightsToBeProcessed.get(i);
-                        if(flight < 0) {
-                            processFlight(flight*-1, year, true);
-                        } else {
-                            processFlight(flight, year, false);
-                        }
-
-                        flightsToBeProcessed.remove(i);
-                    }
-                }
+                int year = Integer.parseInt(fileSplit[1]);
+                airplanes.put(new Text(key), year);
             } else {
                 String[] valueSplit = fileSplit[1].split(",");
                 if(valueSplit.length > 1 && TypeCheckUtil.isInteger(valueSplit[0]) && TypeCheckUtil.isInteger(valueSplit[1])) {
                     int yearOfFlight = Integer.parseInt(valueSplit[0]);
                     boolean hadDelay = Integer.parseInt(valueSplit[1]) > 0;
-                    if(year > 0) {
-                        processFlight(yearOfFlight, year, hadDelay);
+
+                    Text countMapKey;
+                    if(hadDelay) {
+                        countMapKey = new Text(yearOfFlight + ",delay," + key);
                     } else {
-                        if(hadDelay){
-                            flightsToBeProcessed.add(-1*year);
-                        } else {
-                            flightsToBeProcessed.add(year);
-                        }
+                        countMapKey = new Text(yearOfFlight + ",ontime," + key);
                     }
+
+                    int totalFlightsToAdd = 1;
+                    if(countMap.containsKey(countMapKey)) {
+                        totalFlightsToAdd += countMap.get(countMapKey);
+                    }
+                    countMap.put(countMapKey, totalFlightsToAdd);
                 }
             }
         }
@@ -78,24 +56,47 @@ public class OldPlaneDelaysReducer extends Reducer<Text, Text, Text, LongWritabl
 
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
-        LongWritable delayedFlights = new LongWritable(delayedFlightsOver20years);
-        LongWritable totalFlights = new LongWritable(totalFlightsOver20years);
-        context.write(new Text("Total Delayed flights on old planes"), delayedFlights);
-        context.write(new Text("Total flights on old planes"), totalFlights);
 
-//        if(totalFlightsOver20years > 0) {
-//            long average = (long)(new Double(delayedFlightsOver20years)/totalFlightsOver20years * 100);
-//            context.write(new Text("Percent of delayed flights on old planes"), new LongWritable(average));
-//        }
+        long delayedFlightsOver20Years = 0;
+        long totalFlightsOver20Years = 0;
+        long totalFlightsUnder20Years = 0;
+        long delayedFlightsUnder20Years  =0;
 
-        delayedFlights = new LongWritable(delayedFlightsUnder20years);
-        totalFlights =  new LongWritable(totalFlightsUnder20years);
-        context.write(new Text("Total Delayed flights on newer planes"), delayedFlights);
-        context.write(new Text("Total flights on newer planes"), totalFlights);
+        for(Map.Entry<Text, Integer> entry : countMap.entrySet()) {
+            String key = entry.getKey().toString();
+            String[] splitUpKey = key.split(",");
+            if(splitUpKey.length == 3 && TypeCheckUtil.isInteger(splitUpKey[0]) && airplanes.containsKey(new Text(splitUpKey[2]))) {
+                Text airplane = new Text(splitUpKey[2]);
+                int yearOfFlights = Integer.parseInt(splitUpKey[0]);
+                if(Math.abs(yearOfFlights - airplanes.get(airplane)) > 20) {
+                    totalFlightsOver20Years += entry.getValue();
+                    if("delay".equalsIgnoreCase(splitUpKey[1])) {
+                        delayedFlightsOver20Years += entry.getValue();
+                    }
+                } else {
+                    totalFlightsUnder20Years += entry.getValue();
+                    if("delay".equalsIgnoreCase(splitUpKey[1])) {
+                        delayedFlightsUnder20Years += entry.getValue();
+                    }
+                }
+            }
+        }
 
-//        if(totalFlightsUnder20years > 0) {
-//            long average = (long)(new Double(delayedFlightsUnder20years)/totalFlightsUnder20years * 100);
-//            context.write(new Text("Percent of delayed flights on newer planes"), new LongWritable(average));
-//        }
+        context.write(new Text("Total Delayed flights on old planes"), new LongWritable(delayedFlightsOver20Years));
+        context.write(new Text("Total flights on old planes"), new LongWritable(totalFlightsOver20Years));
+
+        if(totalFlightsOver20Years > 0) {
+            long average = (long)(new Double(delayedFlightsOver20Years)/totalFlightsOver20Years * 100);
+            context.write(new Text("Percent of delayed flights on old planes"), new LongWritable(average));
+        }
+
+
+        context.write(new Text("Total Delayed flights on newer planes"), new LongWritable(delayedFlightsUnder20Years));
+        context.write(new Text("Total flights on newer planes"), new LongWritable(totalFlightsUnder20Years));
+
+        if(totalFlightsUnder20Years > 0) {
+            long average = (long)(new Double(delayedFlightsUnder20Years)/totalFlightsUnder20Years * 100);
+            context.write(new Text("Percent of delayed flights on newer planes"), new LongWritable(average));
+        }
     }
 }
